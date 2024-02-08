4dd3d5f3f85cbb9de33a67b4aa070d17f645b017
mahmoud ayman <120199481+mahmoud-aymann@users.noreply.github.com>
2024-02-09 00:53:36 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+const allBoxes = document.querySelectorAll('.box')
+const allTasks = document.querySelectorAll('.task')
+
+allTasks.forEach(task => {
+    task.addEventListener('dragstart', () => {
+        task.classList.add('is-dragging')
+    })
+    task.addEventListener('dragend', () => {
+        task.classList.remove('is-dragging')
+    })
+})
+
+allBoxes.forEach(box => {
+    box.addEventListener('dragover', (e) => {
+        e.preventDefault();
+
+        const curTask = document.querySelector('.is-dragging')
+
+        box.appendChild(curTask)
+    })
+})
+
+// Adding new task
+const form = document.querySelector('#add-form')
+const input = document.querySelector('#todo-input')
+const todoBox = document.querySelector('#to-do')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const newTaskText = input.value;
+
+    if (!newTaskText) return;
+
+    // <p class="task" draggable="true">playing playstation</p>
+    const newTask = document.createElement('p')
+    newTask.classList.add('task')
+    newTask.setAttribute('draggable', 'true')
+    newTask.innerHTML = newTaskText
+
+    newTask.addEventListener('dragstart', () => {
+        newTask.classList.add('is-dragging')
+    })
+    newTask.addEventListener('dragend', () => {
+        newTask.classList.remove('is-dragging')
+    })
+
+    todoBox.appendChild(newTask);
+
+    input.value = '';
+})
